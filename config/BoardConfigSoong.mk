d965b065fb2949842a80393966247ac4005e9d15
Khalvat-M <mmba313@gmail.com>
2022-10-25 06:45:07 +0330

    Revert "config: Remove TARGET_HAS_MEMFD_BACKPORT"
    This reverts commit e020bffce2a1f5a0ba074cede2ce072221a5a6eb.

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -32,6 +32,7 @@ SOONG_CONFIG_lineageGlobalVars += \
     bootloader_message_offset \
     disable_postrender_cleanup \
     gralloc_handle_has_reserved_size \
+    has_memfd_backport \
     needs_netd_direct_connect_rule \
     target_init_vendor_lib \
     target_ld_shim_libs \
@@ -60,6 +61,7 @@ endif
 # Soong bool variables
 SOONG_CONFIG_lineageGlobalVars_disable_postrender_cleanup := $(TARGET_DISABLE_POSTRENDER_CLEANUP)
 SOONG_CONFIG_lineageGlobalVars_gralloc_handle_has_reserved_size := $(TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE)
+SOONG_CONFIG_lineageGlobalVars_has_memfd_backport := $(TARGET_HAS_MEMFD_BACKPORT)
 SOONG_CONFIG_lineageGlobalVars_needs_netd_direct_connect_rule := $(TARGET_NEEDS_NETD_DIRECT_CONNECT_RULE)
 SOONG_CONFIG_lineageGlobalVars_uses_egl_display_array := $(TARGET_USES_EGL_DISPLAY_ARRAY)
 SOONG_CONFIG_lineageNvidiaVars_uses_nvidia_enhancements := $(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)
