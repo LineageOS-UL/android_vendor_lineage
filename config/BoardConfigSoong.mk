0bb1c80a4d0009ba3fff65e8f44280fa64dea53a
Khalvat-M <mmba313@gmail.com>
2022-10-25 06:46:20 +0330

    Revert "config: Remove TARGET_HAS_LEGACY_CAMERA_HAL1"
    This reverts commit 829f975cd8356a18b166f355ed9b3b9316660920.

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -32,6 +32,7 @@ SOONG_CONFIG_lineageGlobalVars += \
     bootloader_message_offset \
     disable_postrender_cleanup \
     gralloc_handle_has_reserved_size \
+    has_legacy_camera_hal1 \
     has_memfd_backport \
     needs_netd_direct_connect_rule \
     target_init_vendor_lib \
@@ -62,6 +63,7 @@ endif
 # Soong bool variables
 SOONG_CONFIG_lineageGlobalVars_disable_postrender_cleanup := $(TARGET_DISABLE_POSTRENDER_CLEANUP)
 SOONG_CONFIG_lineageGlobalVars_gralloc_handle_has_reserved_size := $(TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE)
+SOONG_CONFIG_lineageGlobalVars_has_legacy_camera_hal1 := $(TARGET_HAS_LEGACY_CAMERA_HAL1)
 SOONG_CONFIG_lineageGlobalVars_has_memfd_backport := $(TARGET_HAS_MEMFD_BACKPORT)
 SOONG_CONFIG_lineageGlobalVars_needs_netd_direct_connect_rule := $(TARGET_NEEDS_NETD_DIRECT_CONNECT_RULE)
 SOONG_CONFIG_lineageGlobalVars_uses_egl_display_array := $(TARGET_USES_EGL_DISPLAY_ARRAY)
